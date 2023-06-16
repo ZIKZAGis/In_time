715c25ab2764e61f3c5f753d6ad95b2231f7bf87
Evgeniy <nikolaev_zheka-zik@bk.ru>
2023-06-16 17:24:47 +0300

    adds hints

--- src/screens/Clock/Clock.jsx
+++ src/screens/Clock/Clock.jsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import Header from '../../components/Header/Header';
 import styles from './Clock.module.scss'
 import {FiClock} from 'react-icons/fi'
 
 const Clock = () => {
+  const [time, setTime] = useState(new Date())
+
+  setInterval(() => {
+    setTime(new Date())
+  }, 1000)
+
   return (
     <div className='wrapper'>
       <Header name={`Clock`} icon={<FiClock/>}/>
-      Clock
+      <div className='wrapper'>
+        {time.toLocaleTimeString('ru-Ru')}
+      </div>
     </div>
   );
 }
