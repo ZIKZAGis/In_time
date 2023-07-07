ef9d2f327185893db4864226c4318ac1f0c80e08
Evgeniy <nikolaev_zheka-zik@bk.ru>
2023-07-07 10:16:25 +0300

    inputnumber fix

--- src/components/InputNumber/InputNumber.jsx
+++ src/components/InputNumber/InputNumber.jsx
@@ -7,13 +7,24 @@ const InputNumber = ({name, maxVal, placeHolder, isDisabled = false, setTime}) =
     const setNumber = useCallback(({target}) => {
         let {value, min, max} = target
         value = Math.max(Number(min), Math.min(Number(max), Number(value)))
-        setVal(value)
+
+        setVal(value || '')
         setTime(value)
     }, [setTime])
 
     return (
         <>
-            <input className={styles.input} type="number" name={name} id={name} min={0} max={maxVal} placeholder={placeHolder} onChange={setNumber} value={val} disabled={isDisabled ? true : false}/>
+            <input 
+                className={styles.input}
+                type="number" 
+                name={name} 
+                id={name} 
+                min={0} 
+                max={maxVal} 
+                placeholder={placeHolder} 
+                onChange={setNumber} 
+                value={val}
+                disabled={isDisabled ? true : false}/>
         </>
     )
 }
